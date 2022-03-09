2151abfa605500ed3756e24e4b2a66d0b5eca3f1
Tamm <siusaochemgio@gmail.com>
2022-03-09 23:13:36 +0700

    model sesion

---
+++ WebAPItwe/Models/SessionMeetingModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPItwe.Models
+{
+    public class SessionMeetingModel
+    {
+        public string SessionId { get; set; }
+        public int Slot { get; set; }
+        public string Date { get; set; }
+        public int MaxPerson { get; set; }
+        public string MentorName { get; set; }
+        public string MentorImage { get; set; }
+        public double Price { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectImage { get; set; }
+        public string CafeName { get; set; }
+        public int Status { get; set; }
+        public string CafeDistric { get; set; }
+        public string CafeStreet { get; set; }
+        public List<string> ListMemberImage { get; set; }
+    }
+}
